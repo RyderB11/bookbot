3cb85095e84942e00d3af57ccf083c97fd832c39
RyderB11 <ryderbrock@ymail.com>
2024-02-13 00:56:29 -0800

    updated main to also print word count. not zen of python like, need to add more defining lines for the code.

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ def main():
     with open('books/Frankenstein.txt') as f:
         frakenstein = f.read()
         print(frakenstein)
+        words = frakenstein.split()
+        print(len(words))
+
 main()
