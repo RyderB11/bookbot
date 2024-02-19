6d8413ca54ef4cbad9a1cf2b9fa0eb542bf033b6
RyderB11 <ryderbrock@ymail.com>
2024-02-18 20:40:11 -0800

    this code is cleaner i think, im leaving the counter as maybe an idea to change. this code works, but as boots says, it doesnt work for any other languages. i could initialize a dictionary, and if char is not in the dictionary, set it to 1.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+from collections import Counter
+
 def main():
     with open('books/Frankenstein.txt') as f:
         frankenstein = f.read()
         print(frankenstein)
+
         words = frankenstein.split()
         print(len(words))
+
         lowered_string = frankenstein.lower()
-        from collections import Counter
-        print(Counter(lowered_string))
+        # print(Counter(lowered_string)) this was a counter i used but counted everything to include special characters, i'd rather have just letters for now and practice 
         
-        # count = {'a':,'b':,'c':,'d':,'e':,'f':,'g':,'h':,'i':,'j':,'k':,'l':,'m':,'n':,'o':,'p':,'q':,'r':,'s':,'t':,'u':,'v':,'w':,'x':,'y':,'z':}
+        counter = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
+        for char in lowered_string:
+            if char.isalpha():
+                counter[char] += 1
+        print(counter)
     
 
 main()
