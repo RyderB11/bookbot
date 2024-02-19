c9d52bb29f009a79fc51f08eb9d6679b315b63f1
RyderB11 <ryderbrock@ymail.com>
2024-02-18 20:02:10 -0800

    this code was able to accomplish the lesson, but needs cleaning

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def main():
     with open('books/Frankenstein.txt') as f:
-        frakenstein = f.read()
-        print(frakenstein)
-        words = frakenstein.split()
+        frankenstein = f.read()
+        print(frankenstein)
+        words = frankenstein.split()
         print(len(words))
+        lowered_string = frankenstein.lower()
+        from collections import Counter
+        print(Counter(lowered_string))
+        
+        # count = {'a':,'b':,'c':,'d':,'e':,'f':,'g':,'h':,'i':,'j':,'k':,'l':,'m':,'n':,'o':,'p':,'q':,'r':,'s':,'t':,'u':,'v':,'w':,'x':,'y':,'z':}
+    
 
 main()
